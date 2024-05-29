8cdd660d18f2b420ab8dacacfc49006eea8ad0c5
Sonia Chaboud <sonia.chaboud@gmail.com>
2024-05-29 17:00:02 +0200

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "dashboards#dashboard"
+ # root to: "dashboards#dashboard"
+
+  root to: "products#index"
+
+  get "dashboard", to: "dashboards#dashboard"
+
   get "profile/:id", to: "profiles#profile"
   resources :products, only: [:index, :show, :new, :edit, :update]
   resources :offer_products, only: [:index]
