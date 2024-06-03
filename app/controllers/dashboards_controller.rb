37cb3fad959f734b5e411c0e2cdbc76f368beef6
Sonia Chaboud <sonia.chaboud@gmail.com>
2024-06-03 17:46:13 +0200

    offer creation

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,52 @@
 class DashboardsController < ApplicationController
   def dashboard
   end
+
+  # list of offers current user sent
+  def sent
+
+    @offer_products = OfferProduct.joins(:product, :offer)
+                                  .where(offer: { user_sender: current_user })
+  end
+
+  # current user can delete sent offers
+  def destroyOfferProduct
+    @offer_product = Offer_product.find(params[:id])
+  end
+
+  # received offers from other users to current user
+  def received
+       @products.current_user = Product.find(params[:current_user])
+  end
+
+   # current user must select from other´s users items
+  def select
+    @offer_products = Offer_product.all
+  end
+
+  # current user sends confirmation
+  def confirmRecieved
+    @offer_products = Offer_product.all
+  end
+
+  #  current user can delete received offer
+  def destroyRecieved
+    @offer_product = Offer_product.find(params[:id])
+  end
+
+
+  # confirmation of sent offer with received offer
+  def match
+    @offer_products= Offer_product.all
+    @products.current_user = Product.find(params[:current_user])
+  end
+
+  def destroyMatch
+
+  end
+
+  # users can chat after Match
+  def chat
+  end
+
 end
--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -5,4 +5,17 @@ class OffersController < ApplicationController
   def show
     @offer = Offer.find(params[:id])
   end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @offer = Offer.new(user_sender: current_user, user_receiver: @product.user)
+
+    OfferProduct.create(offer: @offer, product: @product)
+    current_user.products.each do |product|
+      OfferProduct.create(offer: @offer, product: product)
+    end
+
+    @offer.save
+    redirect_to products_path
+  end
 end
