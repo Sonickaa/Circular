37944bc0f84735d713395c37f1484ef60575bea1
Sonia Chaboud <sonia.chaboud@gmail.com>
2024-06-06 09:46:15 +0200

    lets gooo

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_one_attached :photo
-
+  has_many :offer_products, dependent: :destroy
   belongs_to :wishlist, optional: true
+  has_many :product_wishlists, dependent: :destroy
 end
