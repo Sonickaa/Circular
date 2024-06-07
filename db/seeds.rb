1a43c52041a4980740bfa54c5dd5d0b9165b25a6
Sonia Chaboud <sonia.chaboud@gmail.com>
2024-06-07 13:00:01 +0200

    finalllllseeddddsss

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,15 +81,14 @@ products = [
   },
 
   {
-    title: "Nike Air Max 90",
-    user_id: chris.id,
-    category_id: clothing.id,
-    description: "Step up your sneaker game with these iconic Nike Air Max 90s in vibrant turquoise. Featuring premium
-    materials, a cushioned sole, and classic design, these kicks offer both style and comfort. Lightly worn,
-    well-maintained, and ready to add a pop of color to your collection. Don’t miss out!",
-    image_url: "https://sneakernews.com/wp-content/uploads/2019/12/Nike-Air-Max-90-CD0490-104-5.jpg",
+    title: "Fashion workshop",
+    user_id: alex.id,
+    category_id: services.id,
+    description: "No more C&A and H&M, dress like a prince!",
+    image_url: "https://pagesix.com/wp-content/uploads/sites/3/2024/03/bianca-censori-appears-covering-purple-30903956.jpg",
     condition: "used",
-    price: 120
+    price: 100
+
   },
 
   {
@@ -105,32 +104,9 @@ products = [
   },
 
   {
-    title: "Seahaven t-shirt",
-    user_id: chris.id,
-    category_id: clothing.id,
-    description: "Show your love for Seahaven with this Halo of Hurt T-shirt. Lightly worn and in excellent condition,
-    this tee features the iconic album artwork. Perfect for fans and collectors alike, it offers a stylish way
-    to rep your favorite band.",
-    image_url: "https://www.coldcutsmerch.com/cdn/shop/products/SEAHAVENSEH005CURSETHESMILESHIRTMOCK.jpg",
-    condition: "used",
-    price: 40
-  },
-
-  {
-    title: "CR7 Real Madrid tricot",
-    user_id: alex.id,
-    category_id: clothing.id,
-    description: "Own a piece of football history with this CR7 Real Madrid long sleeve jersey. Lightly worn but
-    well-maintained, its perfect for any Ronaldo fan. Stylish, iconic, and ready to wear! Don’t miss out!",
-    image_url: "https://i.ebayimg.com/images/g/SHUAAOSwuwpgYu3p/s-l1200.jpg",
-    condition: "used",
-    price: 80
-  },
-
-  {
     title: "Saturday Sessions",
     user_id: emma.id,
-    category_id: others.id,
+    category_id: services.id,
     description: "We offer these sessions completly for free! Just come to Le Wagon on Saturdays OR thursdays and see what will happen!",
     image_url: "https://res.cloudinary.com/dk5glvhr6/image/upload/fl_preserve_transparency/v1717700890/Screenshot_2024-06-06_at_21.07.50_dci4pn.jpg?_s=public-apps",
     condition: "refurbished",
@@ -138,20 +114,9 @@ products = [
   },
 
   {
-    title: "Personal Styling 😎",
-    user_id: alex.id,
-    category_id: services.id,
-    description: "Trying swipe services? Upgrade your style with personalized flair! Offering expert personal styling
-    to elevate your look and confidence. Let's redefine your wardrobe together!",
-    image_url: "https://images.squarespace-cdn.com/content/v1/5f21b3c0862b9f38f883a002/7934ef35-cdd7-4681-a72d-924094eee139/Men%27s+before+and+after+transformation-will.jpg",
-    condition: "refurbished",
-    price: 85
-  },
-
-  {
     title: "Used Chessboard",
     user_id: josh.id,
-    category_id: others.id,
+    category_id: toys_games.id,
     description: "Check out our old chess board and maybe also play with us!! We also solve coding bugs hehe ♟️",
     image_url: "https://res.cloudinary.com/dk5glvhr6/image/upload/fl_preserve_transparency/v1717700668/Screenshot_2024-06-06_at_21.03.47_yfegqw.jpg?_s=public-apps",
     condition: "used",
@@ -161,7 +126,7 @@ products = [
   {
     title: "Fire ant colony",
     user_id: remi.id,
-    category_id: others.id,
+    category_id: toys_games.id,
     description: "Fire ants are small, aggressive ants known for their painful stings. They are reddish-brown in color
     and are commonly found in warm climates.",
     image_url: "https://res.cloudinary.com/dk5glvhr6/image/upload/fl_preserve_transparency/v1717701185/Screenshot_2024-06-06_at_21.12.20_yg2rd2.jpg?_s=public-apps",
@@ -170,26 +135,14 @@ products = [
   },
 
   {
-    title: "Origami Book",
+    title: "Origami workshop",
     user_id: remi.id,
     category_id: toys_games.id,
     description: "Discover the magic of origami with this amazing used book of me! Perfect for beginners or enthusiasts,
     it's filled with captivating designs and easy-to-follow instructions. Also, you can maybe learn how to code a bird 🗞️",
-    image_url: "https://res.cloudinary.com/dk5glvhr6/image/upload/fl_preserve_transparency/v1717700024/Screenshot_2024-06-06_at_20.53.01_xcyybd.jpg?_s=public-apps",
+    image_url: "https://i.ytimg.com/vi/C2n8BvemmNQ/maxresdefault.jpg",
     condition: "new",
     price: 55
-  },
-
-  {
-    title: "Suculents",
-    user_id: remi.id,
-    category_id: home_appliances.id,
-    description: "Enhance your space with these gently used succulents! Each one carries unique charm and character,
-    adding a touch of greenery to your home. Perfect for those seeking low-maintenance beauty. Grab these pre-loved
-    succulents and bring life to your surroundings!",
-    image_url: "https://i.etsystatic.com/21530912/r/il/64056b/2451976559/il_fullxfull.2451976559_2dpt.jpg",
-    condition: "used",
-    price: 3
   },
 
   {
@@ -208,43 +161,19 @@ products = [
     user_id: tuo.id,
     category_id: sport_wellness.id,
     description: "I'll teach you how to get away from your coding errors AND how to run a marathon!! 🏃🏼🏃🏼🏃🏼",
-    image_url: "https://res.cloudinary.com/dk5glvhr6/image/upload/fl_preserve_transparency/v1717700891/Screenshot_2024-06-06_at_21.07.59_kmgifa.jpg?_s=public-apps",
+    image_url: "https://res.cloudinary.com/dk5glvhr6/image/upload/fl_preserve_transparency/v1717754188/Screenshot_2024-06-07_at_11.55.39_oegcs6.jpg?_s=public-apps",
     condition: "used",
     price: 180
   },
 
   {
-    title: "Walking dog service :)",
+    title: "Spend day with Lucas Jinny :)",
     user_id: tuo.id,
     category_id: services.id,
-    description: "Embark offers professional walking dog services tailored to your furry friend's needs. Our experienced
-    team ensures your dog gets the exercise and attention they deserve, leaving you with peace of mind.
-    Let's keep those tails wagging together!",
-    image_url: "https://upload.wikimedia.org/wikipedia/commons/5/51/Dog_walker_-_Buenos_Aires.jpg",
+    description: "Take Jinny for a walk and get a free coding lesson with the most patient angel nerd!",
+    image_url: "https://i.ibb.co/bF6x1zv/DSC2734.jpg",
     condition: "used",
     price: 20
-  },
-
-  {
-    title: "Mandarin classes",
-    user_id: tuo.id,
-    category_id: services.id,
-    description: "Want to learn some basic Mandarin? Reach me out!",
-    image_url: "https://www.languagebird.com/wp-content/uploads/2021/06/Mandarin-Language-e1629327465771.jpg",
-    condition: "new",
-    price: 65
-  },
-
-  {
-    title: "Virgil's Off White",
-    user_id: tuo.id,
-    category_id: clothing.id,
-    description: "Elevate your style with our gently used Off-White belt. Crafted with impeccable design and quality,
-    it adds a touch of luxury to any outfit. Make a statement with this iconic accessory,
-    perfect for fashion enthusiasts seeking a stylish edge.",
-    image_url: "https://img.vitkac.com/uploads/product_thumb/PASEK%20OMRB012S22%20FAB002-1810/up/2.jpg",
-    condition: "used",
-    price: 145
   },
 
   {
@@ -254,50 +183,13 @@ products = [
     description: "Indulge in relaxation with our professional massage service, available in exchange for your offered
     goods or services. Let our skilled therapists melt away your stress and tension,
     leaving you refreshed and rejuvenated. Experience the ultimate in self-care today.",
-    image_url: "https://i0.wp.com/columbiaintegratedhealth.com/wp-content/uploads/2018/01/columbia-integrated-health-new-westminster-massage-therapy.jpg",
+    image_url: "https://townsquare.media/site/29/files/2011/09/kittenmassage.jpg",
     condition: "used",
-    price: 110
+    price: 30
   },
 
   {
-    title: "Kitty food",
-    user_id: sonia.id,
-    category_id: pet_supplies.id,
-    description: "Treat your feline friend to the finest with gently used Almo Nature cat food.
-    Crafted with premium ingredients,
-    it provides the nourishment and flavor your cat craves. Give your pet the best with Almo Nature,
-    perfect for pampered kitties.",
-    image_url: "https://m.media-amazon.com/images/I/61ZIEm-MRsL.jpg",
-    condition: "used",
-    price: 25
-  },
-
-  {
-    title: "Everlast vintage gloves",
-    user_id: sonia.id,
-    category_id: sport_wellness.id,
-    description: "Get punchy with our pre-loved Everlasting Boxing Gloves! They've survived more rounds than Rocky and
-    still pack a punch. Perfect for knocking out stress or sparring with shadows.
-    Step into the ring of hilarity with these gloves - they've got a punchline of their own!",
-    condition: "used",
-    image_url: "https://i.ebayimg.com/images/g/aRsAAOSwAh9jnUYE/s-l1600.jpg",
-    price: 60
-  },
-
-  {
-    title: "Asus gamer laptop",
-    user_id: sonia.id,
-    category_id: sport_wellness.id,
-    description: "Level up your gaming arsenal with our used ASUS Gamer Laptop. It's seen its fair share of epic battles
-    and late-night raids, but it's ready for more. With specs that could make a dragon jealous,
-    this laptop is your trusty sidekick in virtual adventures. Get ready to slay some pixels!",
-    condition: "used",
-    image_url: "https://classifieds.248am.com/oc-content/uploads/351/55716.jpg",
-    price: 525
-  },
-
-  {
-    title: "Ikea used chair 🇸🇪",
+    title: "Le Wagon borrowed chair",
     user_id: sonia.id,
     category_id: home_appliances.id,
     description: "Introducing the IKEA Shitty Used Chair – because sometimes even the worst seats have stories to
@@ -309,40 +201,35 @@ products = [
   },
 
   {
-    title: "Face mask melumé",
-    user_id: sachini.id,
-    category_id: beauty_personal_care.id,
-    description: "Introducing Melume's latest face mask – your new must-have accessory for both style and safety.
-    Crafted with care and comfort in mind, this mask blends fashion with functionality.
-    With a range of vibrant colors and patterns, you can express your personality while staying protected. Upgrade your
-    mask game with Melume today!",
-    condition: "used",
-    image_url: "https://www.melume-skinscience.com/cdn/shop/files/Produktfotos_1000x1000_NEU_19_1000x.jpg",
-    price: 70
-  },
-
-  {
-    title: "Sri Lankan Chatties 🇱🇰",
-    user_id: sachini.id,
-    category_id: beauty_personal_care.id,
-    description: "
-    Experience the authentic taste of Sri Lankan cuisine with our set of two traditional chatties! Handcrafted by
-    skilled artisans using natural clay, these pots bring the essence of Sri Lankan cooking to
-    your kitchen.",
-    condition: "used",
-    image_url: "https://soulkitchenblog.wordpress.com/wp-content/uploads/2011/12/clay-chatties.jpg",
-    price: 90
-  },
-
-  {
     title: "Learn how to become a BM with Emma",
     user_id: emma.id,
     category_id: beauty_personal_care.id,
     description: "Learn with Emma and her assistant Precious how to become a Batch Manager (Extra points for 👻)",
     condition: "used",
-    image_url: "https://soulkitchenblog.wordpress.com/wp-content/uploads/2011/12/clay-chatties.jpg",
+    image_url: "https://res.cloudinary.com/dk5glvhr6/image/upload/fl_preserve_transparency/v1717754371/Screenshot_2024-06-07_at_11.58.54_vetdbq.jpg?_s=public-apps",
     price: 90
-  }
+  },
+
+  {
+    title: "Mandarin classes",
+    user_id: tuo.id,
+    category_id: services.id,
+    description: "So you can learn the chinese beaver meme.",
+    image_url: "https://img.buzzfeed.com/buzzfeed-static/static/2023-12/13/19/asset/e356f734c542/sub-buzz-488-1702494453-1.png",
+    condition: "used",
+    price: 130
+  },
+
+  {
+    title: "PC for coding",
+    user_id: sachini.id,
+    category_id: electronics.id,
+    description: "Rails and terminal installed. Hanna Montana OS",
+    image_url: "https://live.staticflickr.com/2278/2735682932_012268ef15_b.jpg",
+    condition: "used",
+    price: 1220
+  },
+
 ]
 
 products.each_with_index do |product, index|
